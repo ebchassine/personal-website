a83209afcc6d1df3adf1668e8de1339388da0d0e
Ethan Wong-Chassine <ethanwong-chassine@mac.mynetworksettings.com>
2024-10-27 17:41:24 -0400

    Added social buttoms to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Home, Folder, Image } from 'react-feather';
+import React, { useState } from 'react';
+import { Home, Folder, Image, Linkedin, GitHub, Mail } from 'react-feather';
 
 const iconMap = {
   'about': Home,
@@ -8,6 +8,14 @@ const iconMap = {
 };
 
 function Sidebar({ sections, activeSection, onSectionClick }) {
+    // Used in lower part of sidebar
+    const [emailCopied, setEmailCopied] = useState(false);
+    const handleEmailCopy = () => {
+        navigator.clipboard.writeText('ebchassine@gmail.com');
+        setEmailCopied(true);
+        setTimeout(() => setEmailCopied(false), 2000); // Hide message after 2 seconds
+      };
+    
   return (
     <aside className="w-64 bg-background text-text h-screen flex flex-col">
       <div className="p-6">
@@ -35,8 +43,26 @@ function Sidebar({ sections, activeSection, onSectionClick }) {
           </ul>
         </nav>
       </div>
-      <div className="mt-auto p-6">
-        <p className="text-sm">&copy; 2023 Your Name</p>
+       {/* Social icons section */}
+       <div className="mt-auto p-6">
+        <div className="flex space-x-4 mb-4">
+          {/* LinkedIn Icon */}
+          <a href="https://www.linkedin.com/in/ethan-wong-chassine-1530b1270/" target="_blank" rel="noopener noreferrer">
+            <Linkedin size={20} className="hover:text-linkedin-color" />
+          </a>
+          {/* GitHub Icon */}
+          <a href="https://github.com/ebchassine" target="_blank" rel="noopener noreferrer">
+            <GitHub size={20} className="hover:text-github-color" />
+          </a>
+          {/* Gmail Icon */}
+          <button onClick={handleEmailCopy} className="focus:outline-none">
+            <Mail size={20} className="hover:text-gmail-color" />
+          </button>
+        </div>
+        {/* Notification message */}
+        {emailCopied && <p className="text-sm text-green-500">Email copied to clipboard!</p>}
+        
+        {/* <p className="text-sm">&copy; 2023 Your Name</p> */}
       </div>
     </aside>
   );
